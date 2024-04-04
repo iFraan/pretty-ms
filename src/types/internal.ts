232bca85373de72bfb584d662150d51ea3e92590
Francisco Zorat <franciscozorat@gmail.com>
2024-04-04 09:26:58 -0300

    types

---
+++ src/types/internal.ts
@@ -0,0 +1,11 @@
+export type PrettyMsOptions = {
+    unitCount?: number;
+    colonNotation?: boolean;
+    compact?: boolean;
+    formatSubMilliseconds?: boolean;
+    separateMilliseconds?: boolean;
+    verbose?: boolean;
+    secondsDecimalDigits?: number;
+    millisecondsDecimalDigits?: number;
+    keepDecimalsOnWholeSeconds?: boolean;
+}
