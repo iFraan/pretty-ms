7b1f2842e0ef216b2c3815d0d0033f1cb62af98a
Francisco Zorat <franciscozorat@gmail.com>
2024-04-04 09:30:55 -0300

    more tests

--- test.ts
+++ test.ts
@@ -1,9 +1,12 @@
 import { prettyMs } from './src/index';
 
 const test = async () => {
-    const value = 1000 * 65 * 5 * 10;
+    const value = 1001 * 65 * 5 * 10 * 305;
     console.log(prettyMs(value, { verbose: false }))
+    console.log(prettyMs(value, { verbose: false, compact: true }))
+    console.log(prettyMs(value, { verbose: true, compact: true }))
     console.log(prettyMs(value, { verbose: true }))
+    console.log(prettyMs(value, { verbose: true, formatSubMilliseconds: true }))
 };
 
 test();
