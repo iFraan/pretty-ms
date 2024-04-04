a96e65be1e291f44d529482e1b1ba9933f93d4f2
Francisco Zorat <franciscozorat@gmail.com>
2024-04-04 09:27:07 -0300

    precar test

--- test.ts
+++ test.ts
@@ -1,7 +1,9 @@
-import { } from './src/index';
+import { prettyMs } from './src/index';
 
 const test = async () => {
-
+    const value = 1000 * 65 * 5 * 10;
+    console.log(prettyMs(value, { verbose: false }))
+    console.log(prettyMs(value, { verbose: true }))
 };
 
 test();
