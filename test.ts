cbd48c3e5b413bae21f8044ad00db97889bbd057
Francisco Zorat <franciscozorat@gmail.com>
2024-04-04 10:19:37 -0300

    update test

--- test.ts
+++ test.ts
@@ -1,12 +1,20 @@
 import { prettyMs } from './src/index';
 
 const test = async () => {
-    const value = 1001 * 65 * 5 * 10 * 305;
-    console.log(prettyMs(value, { verbose: false }))
-    console.log(prettyMs(value, { verbose: false, compact: true }))
-    console.log(prettyMs(value, { verbose: true, compact: true }))
-    console.log(prettyMs(value, { verbose: true }))
-    console.log(prettyMs(value, { verbose: true, formatSubMilliseconds: true }))
+    const value = (1003 * 90 * 5 * 10 * 305) - 36000; // random ms
+    const test_data = {
+        default: prettyMs(value),
+        compact: {
+            non_verbose: prettyMs(value, { verbose: false, compact: true }),
+            verbose: prettyMs(value, { verbose: true, compact: true }),
+        },
+        verbose: {
+            default: prettyMs(value, { verbose: true }),
+            with_ms: prettyMs(value, { verbose: true, formatSubMilliseconds: true }),
+        },
+        another_lang: prettyMs(value, { verbose: true, formatSubMilliseconds: true, lang: 'es' })
+    }
+    console.log(test_data)
 };
 
 test();
